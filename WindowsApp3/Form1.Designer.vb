f4b3009b9b790f1b57acae4d3e6dea6c87062539
rafiak <rafal.szczepaniak97@gmail.com>
2017-06-03 15:41:31 +0200

    Pierwszy commit

---
+++ WindowsApp3/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Napięcie"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Częstotliwość"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 174)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(204, 102)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Odbiorniki"
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(7, 67)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(102, 17)
+        Me.CheckBox3.TabIndex = 2
+        Me.CheckBox3.Text = "Kondensator C1"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(6, 43)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(74, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Cewka L1"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(7, 20)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(84, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Rezystor R2"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 282)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(201, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Oblicz"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(117, 34)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(117, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(13, 311)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(204, 251)
+        Me.ListBox1.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(117, 91)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(117, 118)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(117, 145)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "R2 [Ohm]"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 124)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "L1 [H]"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 145)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "C1 [F]"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 574)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ WindowsApp3/Form1.vb
@@ -0,0 +1,209 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FunkcjaDoTegoObwodu()
+
+    End Sub
+    Private Function OporPolaczeniaSzeregowego(PierwszyOdbiornik As Double, DrugiOdbiornik As Double) As Double
+        Return PierwszyOdbiornik + DrugiOdbiornik
+    End Function
+    Private Function PradWGalezi(OporGalezi As Double, NapiecieNaGalezi As Double) As Double
+        Return NapiecieNaGalezi / OporGalezi
+    End Function
+    Private Function NapiecieNaGalezi(OporGalezi As Double, PradWGalezi As Double) As Double
+        Return OporGalezi * PradWGalezi
+    End Function
+    Private Function OporPolaczeniaRownoleglego(ByVal R1 As Double, ByVal R2 As Double, Optional ByVal R3 As Double = -1, Optional ByVal R4 As Double = -1, Optional ByVal R5 As Double = -1, Optional ByVal R6 As Double = -1, Optional ByVal R7 As Double = -1) As Double
+        Dim opory As New List(Of Double) From {R1, R2, R3, R4, R5, R6, R7}
+        Dim Rz As Double
+        Rz = 0
+        For Each r As Double In opory
+            If (r <> -1) Then
+                Rz = Rz + 1 / r
+            End If
+        Next
+        Return 1 / Rz
+    End Function
+    Private Function OporPolaczeniaRownoleglego(ByVal opory As List(Of Double)) As Double
+        Dim Rz As Double
+        Rz = 0
+        For Each r As Double In opory
+            Rz = Rz + 1 / r
+        Next
+        Return 1 / Rz
+    End Function
+    Private Function Zmiana(ByVal poczatekZakresuZmiennosci As Double, ByVal koniecZakresuZmiennosi As Double, ByVal Krok As Double) As List(Of Double)
+        Dim iloscKrokow As Integer
+        iloscKrokow = Math.Floor((koniecZakresuZmiennosi - poczatekZakresuZmiennosci) / Krok)
+        Dim x As Double
+        Dim wynik As New List(Of Double)
+        For i As Integer = 0 To iloscKrokow
+            x = poczatekZakresuZmiennosci + i * Krok
+
+
+            Dim V, f, R2, C, L, R1 As Double
+            R1 = 100
+            Dim opory As New List(Of Double)
+            pobierzDowolnaWartoscTypuDouble(TextBox1, V)
+            If Not pobierzWartoscTypuDoubleRoznaOdZera(TextBox2, f) Then
+
+            End If
+            opory.Add(x)
+            If CheckBox2.Checked Then
+                If Not pobierzDowolnaWartoscTypuDouble(TextBox4, L) Then
+
+                End If
+                opory.Add(2 * Math.PI * f * L)
+            End If
+            If CheckBox3.Checked Then
+                If Not pobierzDowolnaWartoscTypuDouble(TextBox5, C) Then
+
+                End If
+                opory.Add(1 / (2 * Math.PI * f * C))
+            End If
+
+            Dim Rz As Double = OporPolaczeniaSzeregowego(100, OporPolaczeniaRownoleglego(opory))
+
+
+
+            wynik.Add(NapiecieNaGalezi(100, PradWGalezi(Rz, V))) 'w nawiasie podać wyrażenie zwracające wynik dla danej wartości x
+        Next
+        Return wynik
+    End Function
+    Private Sub kopiowanieKoelkcjiDoListBox(ByRef kolekcjaPierwotna As List(Of Double), ByVal kolokcjaWtorna As ListBox.ObjectCollection)
+        kolokcjaWtorna.Clear()
+        For Each x As Double In kolekcjaPierwotna
+            kolokcjaWtorna.Add(x)
+        Next
+    End Sub
+    Private Sub prostyWykres(ByVal wartosci As List(Of Double), ByRef poczatekDziedziny As Double, ByRef koniecDziedziny As Double)
+        Dim Top, Left, Right, Bottom, x1, y1, x2, y2 As Integer
+        Top = 50
+        Bottom = 500
+        Left = 250
+        Right = 700
+        Dim skala, krokow, krok As Double
+        skala = (Bottom - Top) / wartosci.Max
+        krokow = Math.Ceiling((Right - Left) / wartosci.Count)
+        krok = (Right - Left) / wartosci.Count
+        Dim graph As Graphics
+        graph = Me.CreateGraphics
+        graph.Clear(BackColor)
+        Dim p As New Pen(Color.Black, 2)
+        graph.DrawLine(p, Left, Top, Left, Bottom)
+        graph.DrawLine(p, Left, Top, Right, Top)
+        graph.DrawLine(p, Left, Bottom, Right, Bottom)
+        graph.DrawLine(p, Right, Top, Right, Bottom)
+        p.Color = Color.Red
+        For x As Integer = 0 To wartosci.Count - 2
+            x1 = Left + x * krok
+            y1 = Math.Round(Bottom - wartosci(x) * skala)
+            x2 = Left + (x + 1) * krok
+            y2 = Math.Round(Bottom - wartosci(x + 1) * skala)
+            graph.DrawLine(p, x1, y1, x2, y2)
+        Next
+    End Sub
+    Private Sub przecietnyWykres(ByVal wartosci As List(Of Double))
+        Dim Top, Left, Right, Bottom, x1, y1, x2, y2, ox, rozmiarXStrzalki, rozmiarYStrzalki, oxr As Integer
+        Top = 50
+        Bottom = 500
+        Left = 250
+        Right = 1000
+        rozmiarXStrzalki = (Right - Left) / 25
+        rozmiarYStrzalki = (Bottom - Top) / 25
+        Dim skala, krokow, krok As Double
+        skala = (Bottom - Top) / (wartosci.Max - wartosci.Min)
+        krokow = Math.Ceiling((Right - Left) / wartosci.Count)
+        krok = (Right - Left) / wartosci.Count
+        ox = wartosci.Min * skala
+        If (ox > 0) Then
+            oxr = 0
+        Else
+            oxr = ox
+        End If
+
+        Dim graph As Graphics
+        graph = Me.CreateGraphics
+        graph.Clear(BackColor)
+        Dim p As New Pen(Color.Black, 2)
+        graph.DrawLine(p, Left, Top, Left, Bottom)
+        graph.DrawLine(p, Left, Top, Right, Top)
+        graph.DrawLine(p, Left, Bottom, Right, Bottom)
+        graph.DrawLine(p, Right, Top, Right, Bottom)
+
+        graph.DrawLine(p, Left, Bottom + oxr, Right, Bottom + oxr)
+        graph.DrawLine(p, Right, Bottom + oxr, Right - rozmiarXStrzalki, Bottom + oxr - rozmiarYStrzalki)
+        graph.DrawLine(p, Right, Bottom + oxr, Right - rozmiarXStrzalki, Bottom + oxr + rozmiarYStrzalki)
+
+        p.Color = Color.Red
+        For x As Integer = 0 To wartosci.Count - 2
+            x1 = Left + x * krok
+            y1 = Math.Round(Bottom + ox - wartosci(x) * skala)
+            x2 = Left + (x + 1) * krok
+            y2 = Math.Round(Bottom + ox - wartosci(x + 1) * skala)
+            graph.DrawLine(p, x1, y1, x2, y2)
+        Next
+
+    End Sub
+    Private Function pobierzDowolnaWartoscTypuDouble(ByVal wart As TextBox, ByRef liczba As Double) As Boolean
+        Dim wartosc As Double
+        Try
+            wartosc = Double.Parse(wart.Text)
+        Catch
+            MessageBox.Show("Coś poszło nie tak, być może wpisałeś literę zamiast cyfr. Popraw to!")
+            Return False
+        End Try
+        liczba = wartosc
+        Return True
+    End Function
+    Private Function pobierzWartoscTypuDoubleRoznaOdZera(ByVal wart As TextBox, ByRef liczba As Double) As Boolean
+        Dim wartosc As Double
+        Try
+            wartosc = Double.Parse(wart.Text)
+        Catch
+            MessageBox.Show("Coś poszło nie tak, być może wpisałeś literę zamiast cyfr. Popraw to!")
+            Return False
+        End Try
+        If (wartosc = 0) Then
+            MessageBox.Show("Podana wartosc to 0, co jest niedopuszczalne. Popraw to!")
+            Return False
+        Else
+            liczba = wartosc
+            Return True
+        End If
+        liczba = wartosc
+        Return True
+    End Function
+    Private Sub FunkcjaDoTegoObwodu()
+        Dim V, f, R2, C, L, R1 As Double
+        R1 = 100
+        Dim opory As New List(Of Double)
+        If Not pobierzDowolnaWartoscTypuDouble(TextBox1, V) Then
+            Return
+        End If
+        If Not pobierzWartoscTypuDoubleRoznaOdZera(TextBox2, f) Then
+            Return
+        End If
+        If CheckBox1.Checked Then
+            If Not pobierzDowolnaWartoscTypuDouble(TextBox3, R2) Then
+                Return
+            End If
+            opory.Add(R2)
+        End If
+        If CheckBox2.Checked Then
+            If Not pobierzDowolnaWartoscTypuDouble(TextBox4, L) Then
+                Return
+            End If
+            opory.Add(2 * Math.PI * f * L)
+        End If
+        If CheckBox3.Checked Then
+            If Not pobierzDowolnaWartoscTypuDouble(TextBox5, C) Then
+                Return
+            End If
+            opory.Add(1 / (2 * Math.PI * f * C))
+        End If
+
+        Dim Rz As Double = OporPolaczeniaSzeregowego(100, OporPolaczeniaRownoleglego(opory))
+        ListBox1.Items.Add("Napięcie na oporze R1 wynosi" & NapiecieNaGalezi(100, PradWGalezi(Rz, V)))
+        przecietnyWykres(Zmiana(0, R2, 0.1))
+    End Sub
+End Class
